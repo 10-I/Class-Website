bf59fd48345058216adbb0e1b395226269ccf5eb
amazinglySK <shashwat07ktm@gmail.com>
2021-04-12 14:55:09 +0400

    Added the description

---
+++ wow.js
@@ -0,0 +1,19 @@
+function doStuff() {
+  if (
+    localStorage.bio == null ||
+    localStorage.bio == undefined ||
+    localStorage.bio == " "
+  ) {
+    let something = prompt("Hey you enter your description! Or else!!");
+    localStorage.bio = something;
+    document.getElementById("name").innerHTML = localStorage.name;
+    document.getElementById("description").innerHTML = localStorage.getItem(
+      "bio"
+    );
+  } else {
+    document.getElementById("name").innerHTML = localStorage.name;
+    document.getElementById("description").innerHTML = localStorage.bio;
+  }
+}
+
+doStuff();
